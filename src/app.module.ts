df6077470678e1f9ed917d70bc30dd26652c92e2
Thobif <jirayootpandee537@gmail.com>
2022-02-11 10:36:15 +0700

    initial update

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DepartmentModule } from './department/department.module';
+import { ProfileModule } from './profile/profile.module';
+import { PositionModule } from './position/position.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [DepartmentModule, ProfileModule, PositionModule, PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
